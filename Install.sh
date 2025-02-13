a6c083f885e93546e65b309f2dc45a7f5c77d60e
worker <georg_e_boy@outlook.com>
2025-02-14 00:02:06 +1100

    added additional files to repo

--- Install.sh
+++ Install.sh
@@ -1,3 +1,18 @@
-sudo apt update && sudo apt updrade && sudo apt install gh gcc feh make xorg kitty cowsay neovim neofetch libxft-dev libx11-dev libxinerama-dev -y
+sudo apt update && sudo apt upgrade && sudo apt install gh gcc feh make xorg kitty cowsay neovim neofetch libxft-dev libx11-dev libxinerama-dev -y
 
 git clone https://git.suckless.org/dwm && git clone https://git.suckless.org/dmenu
+
+mv dwm ~/dwm && mv dmenu ~/dmenu && mv abstract.png ~/abstract.png
+
+cd ~/ && mkdir .config && cd .config && mkdir kitty && mv kitty.conf ~/.config/kitty/kitty.config
+
+cd ~/Linux_config_files && mv config.h ~/dwm/config.h && cd ~/dwm/ && sudo make clean install
+
+touch ~/.remaps.sh
+touch ~/.xinitrc
+
+echo "exec dwm" >> ~/.xinitrc && echo "feh --bg-center ~/abstract.png" >> ~/.xinitrc && echo "xset -b" >> ~/.xinitrc
+
+echo "source .remaps.sh" >> ~/.bashrc
+
+echo "alias cls=\"clear\"" >> ~/.remaps.sh && echo "alias exe=\"./EXE\"" >> ~/.remaps.sh && echo "alias EXE=\"./EXE\"" >> ~/.remaps.sh
