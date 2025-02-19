f3b5fafe5e823444a7e0fe9186a2b4602adfca6f
worker <ethan.todd@gs.adventist.edu.au>
2025-02-20 09:06:50 +1100

    added ranger to install list

--- Install.sh
+++ Install.sh
@@ -1,3 +1,3 @@
-sudo apt update && sudo apt upgrade && sudo apt install gh gcc feh make xorg kitty cowsay neovim neofetch libxft-dev libx11-dev libxinerama-dev -y
+sudo apt update && sudo apt upgrade && sudo apt install gh gcc feh make xorg kitty cowsay neovim neofetch ranger libxft-dev libx11-dev libxinerama-dev -y
 
 git clone https://git.suckless.org/dwm && git clone https://git.suckless.org/dmenu
