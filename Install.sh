454cfb76d2426331683cbab7f137b28ab8606397
ExtremeYeti1267 <124556890+ExtremeYeti1267@users.noreply.github.com>
2025-02-20 20:39:43 +1100

    Update Install.sh
    oh wow why was this sooooooooooooo hard

--- Install.sh
+++ Install.sh
@@ -1,15 +1,12 @@
-<<<<<<< HEAD
 sudo apt update && sudo apt upgrade && sudo apt install gh gcc feh make xorg kitty cowsay neovim neofetch libxft-dev libx11-dev libxinerama-dev -y
-=======
+
 sudo apt update && sudo apt upgrade && sudo apt install gh gcc feh make xorg kitty cowsay neovim neofetch ranger libxft-dev libx11-dev libxinerama-dev -y
->>>>>>> f3b5fafe5e823444a7e0fe9186a2b4602adfca6f
 
 git clone https://git.suckless.org/dwm && git clone https://git.suckless.org/dmenu
 
 mv dwm ~/dwm && mv dmenu ~/dmenu && mv abstract.png ~/abstract.png
 
 cd ~/ && mkdir .config && cd .config && mkdir kitty && mv kitty.conf ~/.config/kitty/kitty.config
-
 cd ~/Linux_config_files && mv config.h ~/dwm/config.h && cd ~/dwm/ && sudo make clean install
 
 touch ~/.remaps.sh
