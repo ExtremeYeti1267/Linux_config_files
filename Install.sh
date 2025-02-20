cf63ab33a4b233541640a0ca87e8458c2ded06e0
ExtremeYeti1267 <georg_e_boy@icloud.com>
2025-02-20 20:49:29 +1100

    fixed the duplicate lines

--- Install.sh
+++ Install.sh
@@ -1,19 +1,15 @@
-sudo apt update && sudo apt upgrade && sudo apt install gh gcc feh make xorg kitty cowsay neovim neofetch libxft-dev libx11-dev libxinerama-dev -y
-
 sudo apt update && sudo apt upgrade && sudo apt install gh gcc feh make xorg kitty cowsay neovim neofetch ranger libxft-dev libx11-dev libxinerama-dev -y
 
 git clone https://git.suckless.org/dwm && git clone https://git.suckless.org/dmenu
 
 mv dwm ~/dwm && mv dmenu ~/dmenu && mv abstract.png ~/abstract.png
 
-cd ~/ && mkdir .config && cd .config && mkdir kitty && mv kitty.conf ~/.config/kitty/kitty.config
-cd ~/Linux_config_files && mv config.h ~/dwm/config.h && cd ~/dwm/ && sudo make clean install
+mkdir ~/.config/kitty && mv kitty.conf ~/.config/kitty/kitty.config
+mv config.h ~/dwm/config.h && cd ~/dwm/ && sudo make clean install
 
 touch ~/.remaps.sh
 touch ~/.xinitrc
 
 echo "exec dwm" >> ~/.xinitrc && echo "feh --bg-center ~/abstract.png" >> ~/.xinitrc && echo "xset -b" >> ~/.xinitrc
-
 echo "source .remaps.sh" >> ~/.bashrc
-
 echo "alias cls=\"clear\"" >> ~/.remaps.sh && echo "alias exe=\"./EXE\"" >> ~/.remaps.sh && echo "alias EXE=\"./EXE\"" >> ~/.remaps.sh
