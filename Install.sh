89dd4019b45a815c1e0734c73e44b7c350296510
worker <ethan.todd@gs.adventist.edu.au>
2025-02-24 14:22:14 +1100

    changed abstract.png to abstract.jpg

--- Install.sh
+++ Install.sh
@@ -2,7 +2,7 @@ sudo apt update && sudo apt upgrade && sudo apt install gh gcc feh make xorg kit
 
 git clone https://git.suckless.org/dwm && git clone https://git.suckless.org/dmenu
 
-mv dwm ~/dwm && mv dmenu ~/dmenu && mv abstract.png ~/abstract.png
+mv dwm ~/dwm && mv dmenu ~/dmenu && mv abstract.jpg ~/abstract.jpg
 
 mkdir ~/.config/kitty && mv kitty.conf ~/.config/kitty/kitty.config
 mv config.h ~/dwm/config.h && cd ~/dwm/ && sudo make clean install
