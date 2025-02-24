bcaf6bb873ddeabb280dcf26f3c80d23e4350b69
worker <ethan.todd@gs.adventist.edu.au>
2025-02-24 15:18:42 +1100

    v2 installer

--- Install.sh
+++ Install.sh
@@ -1,11 +1,12 @@
 sudo apt update && sudo apt upgrade && sudo apt install gh gcc feh make xorg kitty cowsay neovim neofetch ranger libxft-dev libx11-dev libxinerama-dev -y
 
-mkdir -p ~/.config/kitty && mv ~/Linux_config_files/kitty.conf ~/.config/kitty/kitty.config
-mv ~/Linux_config_files/config.h ~/dwm/config.h && cd ~/dwm/ && sudo make clean install
+mv dwm ~/dwm && mv dmenu ~/dmenu && mv .wallpaper ~/.wallpaper && mv .config ~/.config
+
+cd ~/dwm && sudo make clean install && cd ~/dmenu && sudo make clean install 
 
 touch ~/.remaps.sh
 touch ~/.xinitrc
 
-echo "exec dwm" >> ~/.xinitrc && echo "feh --bg-center ~/abstract.jpg" >> ~/.xinitrc && echo "xset -b" >> ~/.xinitrc
+echo "exec dwm" >> ~/.xinitrc && echo "feh --bg-center ~/.wallpaper/abstract.jpg" >> ~/.xinitrc && echo "xset -b" >> ~/.xinitrc
 echo "source .remaps.sh" >> ~/.bashrc
 echo "alias cls=\"clear\"" >> ~/.remaps.sh && echo "alias exe=\"./EXE\"" >> ~/.remaps.sh && echo "alias EXE=\"./EXE\"" >> ~/.remaps.sh
