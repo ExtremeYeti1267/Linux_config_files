a5733aefaeca652a1d8354e87087340a2b23ca8d
worker <ethan.todd@gs.adventist.edu.au>
2025-02-28 11:58:22 +1100

    i dunno why its not working

--- Install.sh
+++ Install.sh
@@ -1,13 +1,13 @@
 sudo apt update && sudo apt upgrade && sudo apt install gh gcc feh make xorg kitty cowsay neovim neofetch ranger chromium libxft-dev libx11-dev libxinerama-dev -y
 
-mv dwm ~/dwm && mv dmenu ~/dmenu && mv wallpaper ~/wallpaper && mv .config ~/.config && mv .fehbg ~/.fehbg
+mv dwm ~/dwm && mv dmenu ~/dmenu && mv wallpaper ~/wallpaper && mv .config ~/.config
 
 cd ~/dwm && sudo make clean install && cd ~/dmenu && sudo make clean install 
 
 touch ~/.remaps.sh
 touch ~/.xinitrc
 
-echo "exec dwm" >> ~/.xinitrc && echo "~/.fehbg" >> ~/.xinitrc && echo "xset -b" >> ~/.xinitrc
+echo "exec dwm" >> ~/.xinitrc && echo "feh --bg-fill ~/wallpaper/abstract.jpg" >> ~/.xinitrc && echo "xset -b" >> ~/.xinitrc
 echo "source .remaps.sh" >> ~/.bashrc
 echo "alias cls=\"clear\"" >> ~/.remaps.sh && echo "alias exe=\"./EXE\"" >> ~/.remaps.sh && echo "alias EXE=\"./EXE\"" >> ~/.remaps.sh
 
