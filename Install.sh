2e3452eea76c23206a1a05f5031f575e2882c1a4
worker <ethan.todd@gs.adventist.edu.au>
2025-03-03 12:34:40 +1100

    removed dwm and dmenu repos from my repo and changed kitty.config to kitty.conf to fix kitty config file naming

--- Install.sh
+++ Install.sh
@@ -1,6 +1,10 @@
 sudo apt update && sudo apt upgrade && sudo apt install gh gcc feh make xorg kitty cowsay neovim neofetch ranger chromium libxft-dev libx11-dev libxinerama-dev -y
 
-mv dwm ~/dwm && mv dmenu ~/dmenu && mv wallpaper ~/wallpaper && mv .config ~/.config
+mv wallpaper ~/wallpaper && mv .config ~/.config
+
+cd ~/ && git clone https://git.suckless.org/dwm && git clone https://git.suckless.org/dmenu
+
+mv ~/Linux_config_files/dwmconfig.h ~/dwm/config.h && mv ~/Linux_config_files/dmenuconfig.h ~/dmenu/config.h
 
 cd ~/dwm && sudo make clean install && cd ~/dmenu && sudo make clean install 
 
