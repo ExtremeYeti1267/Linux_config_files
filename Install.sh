ebf97d2bb28a4b70550c6620e875a2fc5690addf
ExtremeYeti1267 <georg_e_boy@icloud.com>
2025-03-05 17:07:19 +1100

    I will try just using the picom that is installable with apt

--- Install.sh
+++ Install.sh
@@ -4,15 +4,11 @@ sudo apt update && sudo apt upgrade
 
 #   Installing required setup repos   >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
-sudo apt install python3 meson cmake ninja-build gcc feh make xorg libxft-dev libx11-dev libxinerama-dev -y
-
-#   Installing picom dependancies   >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-
-sudo apt install libconfig-dev libdbus-1-dev libegl-dev libev-dev libgl-dev libepoxy-dev libpcre2-dev libpixman-1-dev libx11-xcb-dev libxcb1-dev libxcb-composite0-dev libxcb-damage0-dev libxcb-glx0-dev libxcb-image0-dev libxcb-present-dev libxcb-randr0-dev libxcb-render0-dev libxcb-render-util0-dev libxcb-shape0-dev libxcb-util-dev libxcb-xfixes0-dev meson ninja-build uthash-dev -y
+sudo apt install gcc feh make xorg libxft-dev libx11-dev libxinerama-dev -y
 
 #   Installing other repos    >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
-sudo apt install kitty htop cowsay neovim neofetch ranger chromium gh -y
+sudo apt install python3 kitty htop cowsay neovim neofetch ranger chromium gh -y
 
 #   Creating directories	  >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
@@ -20,7 +16,7 @@ mkdir ~/Programming
 
 #   Cloning applications    >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
-cd ~/ && git clone https://git.suckless.org/dwm && git clone https://git.suckless.org/dmenu && git clone https://github.com/yshui/picom
+cd ~/ && git clone https://git.suckless.org/dwm && git clone https://git.suckless.org/dmenu
 
 #   Moving config files	    >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
@@ -35,13 +31,9 @@ mv ~/Linux_config_files/.config ~/.config
 cd ~/dwm && sudo make clean install
 cd ~/dmenu && sudo make clean install 
 
-#   Building picom    >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-
-cd ~/picom && meson setup --buildtype=release build && ninja -C build && ninja -C build install
-
 #   File creation & appends   >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
-echo "feh --bg-fill -z ~/wallpaper/ &" >> ~/.xinitrc && echo "exec --no-startup-id picom --config ~/.config/picom/picom.conf -b" >> ~/.xinitrc && echo "xset -b &" >> ~/.xinitrc && echo "exec dwm" >> ~/.xinitrc 
+echo "feh --bg-fill -z ~/wallpaper/ &" >> ~/.xinitrc && echo "xset -b &" >> ~/.xinitrc && echo "exec dwm" >> ~/.xinitrc 
 echo "source ~/.remaps.sh" >> ~/.bashrc
 
 #   End of install    >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
